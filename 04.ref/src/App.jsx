d301b181588c0c3dbff7e2dad945ae51bc27b2e3
윤선현 <ysh4776@naver.com>
2025-06-09 16:53:07 +0900

    first commit

---
+++ 04.ref/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import ImgChangeExam from './components/ImgChangeExam'
+import LoginForm from './components/LoginForm'
+
+function App() {
+
+  return (
+    <>
+    {/* Ref 실습1 */}
+    {/* <RefExam/> */}
+
+    {/* Ref 실습2 */}
+    {/* <LoginForm/> */}
+
+    {/* Ref 실습3 */}
+    <ImgChangeExam/>
+    </>
+  )
+}
+
+export default App
---
+++ 04.ref/src/components/ImgChangeExam.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ImgChangeExam = () => {
+
+    if()
+
+    return (
+        <div>
+            <h1>희망하는 사진이 있으신가요?</h1>
+            <form>
+                <p>
+                    <input type="text" />
+                    <button>변경</button>
+                </p>
+                <img style={{
+                    width: "150px"
+                }} src='src/assets/img/쿼카.png'></img>
+            </form>
+        </div>
+    )
+}
+
+export default ImgChangeExam
